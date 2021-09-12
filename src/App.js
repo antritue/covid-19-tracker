c3d5fca302679a5e1966911a9682a103eec45317
tuean172 <tan@fr.scc.com>
2021-09-12 23:57:00 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,82 +1,76 @@
-import { FormControl, Select, MenuItem, Card, CardContent } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import InfoBox from './InfoBox'
-import LineGraph from './LineGraph';
-import Map from './Map'
-import Table from './Table'
-import { printStats, sortData } from './util';
-import "leaflet/dist/leaflet.css"
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import InfoBox from "./InfoBox";
+import LineGraph from "./LineGraph";
+import Map from "./Map";
+import Table from "./Table";
+import { printStats, sortData } from "./util";
+import "leaflet/dist/leaflet.css";
 
 function App() {
-  const [countries, setCountries] = useState([])
-  const [country, setCountry] = useState('worldwide')
-  const [countryInfo, setCountryInfo] = useState({})
-  const [tableData, setTableData] = useState([])
-  const [mapCenter, setMapCenter] = useState([10, 8])
-  const [mapZoom, setMapZoom] = useState(2)
-  const [mapCountries, setMapCountries] = useState([])
-  const [casesType, setCasesType] = useState('cases')
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState([10, 8]);
+  const [mapZoom, setMapZoom] = useState(2);
+  const [mapCountries, setMapCountries] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
 
-  // worldwide cards info 
+  // worldwide cards info
   useEffect(() => {
     const getWorldWideData = async () => {
-      await fetch('https://disease.sh/v3/covid-19/all')
-      .then((response) => response.json())
-      .then((data) => {
-        setCountryInfo(data)
-      })
-    }
-    getWorldWideData()
-  }, [])
+      const response = await fetch("https://disease.sh/v3/covid-19/all");
+      const data = await response.json();
+      setCountryInfo(data);
+    };
+    getWorldWideData();
+  }, []);
 
   // dropdown and table info
   useEffect(() => {
-    const getCountriesData = async () =>{
-      await fetch('https://disease.sh/v3/covid-19/countries')
-      .then(((resoponse) => resoponse.json()))
-      .then((data) => {
-        const countries = data.map((country) => (
-          {
-            name: country.country,
-            value: country.countryInfo._id
-          }
-        ))
-        
-        const sortedData = sortData(data)
-        setTableData(sortedData)
-        setCountries(countries)
-        setMapCountries(data)
-      })
+    const getCountriesData = async () => {
+      const response = await fetch("https://disease.sh/v3/covid-19/countries");
+      const data = await response.json();
+      const countries = data.map((country) => ({
+        name: country.country,
+        value: country.countryInfo._id,
+      }));
+      const sortedData = sortData(data);
+      setTableData(sortedData);
+      setCountries(countries);
+      setMapCountries(data);
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+
+    if (countryCode === "worldwide") {
+      const url = "https://disease.sh/v3/covid-19/all";
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountryInfo(data);
+      setMapCenter([10, 8]);
+      setMapZoom(2);
+    } else {
+      const url = `https://disease.sh/v3/covid-19/countries/${countryCode}?yesterday=true`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountryInfo(data);
+      setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+      setMapZoom(4);
     }
-    getCountriesData()
-  }, [])
-
-  const onCountryChange = async (event) =>{
-    const countryCode = event.target.value
-    setCountry(countryCode)
-
-    if(countryCode==='worldwide'){
-      const url = 'https://disease.sh/v3/covid-19/all'
-      await fetch(url)
-      .then((resoponse) => resoponse.json())
-      .then((data) => {
-        setCountryInfo(data)
-        setMapCenter([10, 8])
-        setMapZoom(2)
-      })
-    } else{
-      const url = `https://disease.sh/v3/covid-19/countries/${countryCode}?yesterday=true`
-      await fetch(url)
-      .then((resoponse) => resoponse.json())
-      .then((data) => {
-        setCountryInfo(data)
-        setMapCenter([data.countryInfo.lat, data.countryInfo.long])
-        setMapZoom(4)
-      })
-    }
-  }
-
+  };
 
   return (
     <>
@@ -84,60 +78,63 @@ function App() {
         <div className="app__left">
           <div className="app__header">
             <h1>COVID-19 tracker</h1>
-            <FormControl className='app__dropdown'>
-              <Select 
-                variant='outlined' 
+            <FormControl className="app__dropdown">
+              <Select
+                variant="outlined"
                 value={country}
                 onChange={onCountryChange}
               >
-                <MenuItem value='worldwide'>Worldwide</MenuItem>
-                {
-                  countries.map(country => (
-                    <MenuItem value={country.value}>{country.name}</MenuItem>
-                  ))
-                }
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                {countries.map((country) => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))}
               </Select>
             </FormControl>
           </div>
 
           <div className="app__stats">
-            <InfoBox 
-              active = {casesType === 'cases'}
-              casesType = {casesType}
-              onClick = {() => setCasesType('cases')}
-              title='Corona virus case'
+            <InfoBox
+              active={casesType === "cases"}
+              casesType={casesType}
+              onClick={() => setCasesType("cases")}
+              title="Corona virus case"
               cases={printStats(countryInfo.todayCases)}
-              total={printStats(countryInfo.cases)} 
+              total={printStats(countryInfo.cases)}
             />
-            <InfoBox 
-              active = {casesType === 'recovered'}
-              casesType = {casesType}
-              onClick = {() => setCasesType('recovered')}
-              title='Recovered' 
-              cases={printStats(countryInfo.todayRecovered)} 
-              total={printStats(countryInfo.recovered)} />
-            <InfoBox 
-              active = {casesType === 'deaths'}
-              casesType = {casesType}
-              onClick = {() => setCasesType('deaths')}
-              title='Deaths' 
-              cases={printStats(countryInfo.todayDeaths)} 
-              total={printStats(countryInfo.deaths)} 
+            <InfoBox
+              active={casesType === "recovered"}
+              casesType={casesType}
+              onClick={() => setCasesType("recovered")}
+              title="Recovered"
+              cases={printStats(countryInfo.todayRecovered)}
+              total={printStats(countryInfo.recovered)}
+            />
+            <InfoBox
+              active={casesType === "deaths"}
+              casesType={casesType}
+              onClick={() => setCasesType("deaths")}
+              title="Deaths"
+              cases={printStats(countryInfo.todayDeaths)}
+              total={printStats(countryInfo.deaths)}
             />
           </div>
 
-          <Map casesType={casesType} center={mapCenter} zoom={mapZoom} countries={mapCountries}/>
+          <Map
+            casesType={casesType}
+            center={mapCenter}
+            zoom={mapZoom}
+            countries={mapCountries}
+          />
         </div>
-        
+
         <Card className="app__right">
           <CardContent>
             <h3>Live Cases by Country</h3>
-            <Table countries={tableData}/>
+            <Table countries={tableData} />
             <h3>Worlwide new {casesType}</h3>
-            <LineGraph casesType={casesType}/>
+            <LineGraph casesType={casesType} />
           </CardContent>
         </Card>
-        
       </div>
       <div className="footer">
         <p>Made by Tue An - 2021</p>
--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,88 +1,88 @@
-import numeral from 'numeral'
-import React, { useEffect, useState } from 'react'
-import {Line} from 'react-chartjs-2'
+import numeral from "numeral";
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
 
 const casesTypeColors = {
-    cases:{
-        hex: '#CC1034',
-        backgroundColor: '#e395a1'
-    },
-    recovered:{
-        hex: '#7dd71d',
-        backgroundColor: '#c3e598'
-    },
-    deaths:{
-        hex: '#6f3b5d',
-        backgroundColor: '#bda7b1'
-    },
-}
+  cases: {
+    hex: "#CC1034",
+    backgroundColor: "#e395a1",
+  },
+  recovered: {
+    hex: "#7dd71d",
+    backgroundColor: "#c3e598",
+  },
+  deaths: {
+    hex: "#6f3b5d",
+    backgroundColor: "#bda7b1",
+  },
+};
 
-const buildChartData = (data, casesType) =>{
-    let chartData = []
-    let lastDataPoint;
-    for(let date in data[casesType]) {
-        if(lastDataPoint){
-            let newCases = data[casesType][date] - lastDataPoint
-            const newDataPoint = {
-                x: date,
-                y: newCases >= 0 ? newCases : 0
-            }
-            chartData.push(newDataPoint)
-        }
-        lastDataPoint = data[casesType][date]
+const buildChartData = (data, casesType) => {
+  let chartData = [];
+  let lastDataPoint;
+  for (let date in data[casesType]) {
+    if (lastDataPoint) {
+      let newCases = data[casesType][date] - lastDataPoint;
+      const newDataPoint = {
+        x: date,
+        y: newCases >= 0 ? newCases : 0,
+      };
+      chartData.push(newDataPoint);
     }
-    return chartData
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+function LineGraph({ casesType }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=45")
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        const chartData = buildChartData(data, casesType);
+        setData(chartData);
+      });
+  }, [casesType]);
+
+  return (
+    <div className="app__graph">
+      {data?.length > 0 && (
+        <Line
+          data={{
+            datasets: [
+              {
+                label: casesType,
+                data: data,
+                borderColor: casesTypeColors[casesType].hex,
+                backgroundColor: casesTypeColors[casesType].backgroundColor,
+              },
+            ],
+          }}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                ticks: {
+                  callback: function (value) {
+                    return numeral(value).format("0a");
+                  },
+                },
+              },
+            },
+            animation: false,
+            plugins: {
+              legend: {
+                display: false,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
-function LineGraph({casesType}) {
-    const [data, setData] = useState({})
-
-    useEffect(() => {
-        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=30')
-        .then((response) => response.json())
-        .then((data) =>{
-            // console.log(data);
-            const chartData = buildChartData(data, casesType)
-            setData(chartData)
-        })
-    }, [casesType])
-
-    return (
-        <div className='app__graph'>
-            {data?.length >0 &&(
-                <Line 
-                    data = {{
-                        datasets: [
-                            {
-                                label: casesType,
-                                data: data,
-                                borderColor: casesTypeColors[casesType].hex,
-                                backgroundColor: casesTypeColors[casesType].backgroundColor
-                            }
-                        ]
-                    }}
-                    options = {{
-                        maintainAspectRatio: false,
-                        scales:{
-                            y: {
-                                ticks:{
-                                    callback: function(value){
-                                        return numeral(value).format('0a')
-                                    }
-                                }
-                            }
-                        },
-                        animation: false,
-                        plugins: {
-                            legend: {
-                                display: false,
-                            }
-                        }
-                    }}
-                />
-            )}
-        </div>
-    )
-}
-
-export default LineGraph
+export default LineGraph;
